f30425499df130e59abf6ba8109b6c92468858ec
Maury2001 <mmakira01@gmail.com>
2024-04-10 16:30:23 +0300

    new routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Suspense, lazy } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Switch} from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 
@@ -10,30 +10,39 @@ const Programs = lazy(() => import("./pages/programs"));
 
 function App() {
   return (
-    <>
+  
       <BrowserRouter>
         <NavBar />
         <Suspense
           fallback={
+            <section className=" w-full h-screen bg-white flex justify-center">
             <div class="flex flex-row gap-2">
               <div class="w-4 h-4 rounded-full bg-blue-700 animate-bounce"></div>
               <div class="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:-.3s]"></div>
               <div class="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:-.5s]"></div>
             </div>
+            </section>
           }
         >
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/programs" element={<Programs />} />
-            {/* Add more routes as needed */}
-            {/* Note: Make sure to include the element prop with the corresponding component */}
-          </Routes>
+          <Switch>
+            <Route exact path="/" >
+              <Home/>
+            </Route>
+            <Route path="/about">
+              <About/>
+            </Route>
+            <Route path="/blog">
+              <Blog/>
+            </Route>
+            <Route path="/programs">
+              <Programs/>
+            </Route>
+           
+            </Switch>
         </Suspense>
         <Footer />
       </BrowserRouter>
-    </>
+    
   );
 }
 
